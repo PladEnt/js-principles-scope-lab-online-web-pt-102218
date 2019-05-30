2287e994faa84663cead5355119484875404a3c5
Joshua D. Forrest <joshuadforrest@gmail.com>
2019-05-30 15:10:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your solution in this file!
 var customerName = 'bob'
 
-upperCaseCustomerName(name)
-
-end
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase()
+}
